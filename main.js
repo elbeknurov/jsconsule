46e71e5916651b2e2b9b8a415d21a7f332bb88b6
elbeknurov <elbeknurov11@gmail.com>
2022-11-15 18:34:26 +0500

    js

---
+++ main.js
@@ -0,0 +1,43 @@
+alert(
+  "Assalomu alaykum saytga xush kelibsiz. \nRo`yxatdan o`tish uchun arizani to`ldiring."
+);
+
+var name = prompt("Ismingizni kiriting");
+// console.log(name);
+var surname = prompt("Familyangizni kiriting");
+// console.log(surname);
+var age = prompt("Yoshingizni kiriting");
+// console.log(age);
+var place = prompt("Yashash manzilingizni kiriting");
+// console.log(place);
+var job = prompt("Siz hozirgi vaqtda qayerda ishlaysiz yoki o`qiysizmi?");
+// console.log(job);
+var tel = prompt("telefon raqamingizni kiriting");
+// console.log(tel);
+var email = prompt("email manzilingizni kiriting");
+// console.log(email);
+alert(
+  `${name} ${surname} siz royxatga olindingiz tez orada siz bilan boglanishadi`
+);
+
+var a = 10;
+var b = 5;
+console.log(a + b); /// 1 misol
+
+var a = 25;
+var b = 15;
+var result = a - b;
+console.log(result); /// 2 misol
+
+var a = 10;
+var b = 5;
+var c = a - b;
+console.log(c); /// 3 misol
+
+var a = 10;
+var b = 3;
+console.log(a % b); /// 4 misol
+
+var a = 2;
+var b = 10;
+console.log(a ** b); /// 5 misol
